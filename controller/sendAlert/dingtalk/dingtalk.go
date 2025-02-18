99c64cf6810e11baed448ca47c28f68b08702e89
南宫乘风 <1794748404@qq.com>
2025-02-18 15:12:29 +0800

    feat: 增加钉钉告警功能，重构README.md文件

--- controller/sendAlert/dingtalk/dingtalk.go
+++ controller/sendAlert/dingtalk/dingtalk.go
@@ -3,44 +3,62 @@ package dingtalk
 import (
 	"fmt"
 	"heapdump_watcher/setting"
-	"io"
+	"io/ioutil"
 	"net/http"
 	"strings"
 )
 
-func SendDingTalk() {
-	url := fmt.Sprintf("https://oapi.dingtalk.com/robot/send?access_token=%s", setting.Conf.AlarmMedium.DingTalkToken)
+func SendDingTalk(msg, env, appName, ossURL string) error {
+	webHook := fmt.Sprintf("https://oapi.dingtalk.com/robot/send?access_token=%s", setting.Conf.AlarmMedium.DingTalkToken)
 
-	payload := strings.NewReader(`{
-    "msgtype": "link", 
-    "link": {
-        "text": "这个即将发布的新版本, 创始人xx称它为红树林。而在此之前, 每当面临重大升级, 产品经理们都会取一个应景的代号, 这一次, 为什么是红树林", 
-        "title": "时代的火车向前开", 
-        "picUrl": "", 
-        "messageUrl": "https://www.dingtalk.com/s?__biz=MzA4NjMwMTA2Ng=="
-    }
-}`)
+	// 构造告警标题和 Markdown 内容
+	title := fmt.Sprintf("【告警】%s", appName)
+	markdownText := fmt.Sprintf(
+		"### 告警通知\n\n"+
+			"> **环境**：%s\n\n"+
+			"> **应用**：%s\n\n"+
+			"> **OSS地址**：[点击查看](%s)\n\n"+
+			"> **告警信息**：%s\n",
+		env, appName, ossURL, msg)
 
+	// 构造 JSON 格式的消息体（使用 Markdown 格式）
+	content := fmt.Sprintf(`{
+	"msgtype": "markdown",
+	"markdown": {
+		"title": "%s",
+		"text": "%s"
+	},
+	"at": {
+		"atMobiles": [
+			""
+		],
+		"isAtAll": false
+	}
+}`, title, markdownText)
+
+	// 创建 POST 请求
+	req, err := http.NewRequest("POST", webHook, strings.NewReader(content))
+	if err != nil {
+		fmt.Println("创建请求错误：", err)
+		return err
+	}
+
+	// 设置请求头
+	req.Header.Set("Content-Type", "application/json; charset=utf-8")
 	client := &http.Client{}
-	req, err := http.NewRequest("POST", url, payload)
-
+	resp, err := client.Do(req)
 	if err != nil {
-		fmt.Println(err)
-		return
+		fmt.Println("发送请求错误：", err)
+		return err
 	}
-	req.Header.Add("Content-Type", "application/json")
+	defer resp.Body.Close()
 
-	res, err := client.Do(req)
+	// 读取响应
+	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
-		fmt.Println(err)
-		return
+		fmt.Println("读取响应错误：", err)
+		return err
 	}
-	defer res.Body.Close()
-
-	body, err := io.ReadAll(res.Body)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	fmt.Println(string(body))
+	fmt.Println("响应内容：", string(body))
+	return nil
 }
